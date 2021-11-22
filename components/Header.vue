37e8dee690e434405cf565466aaf0f9e78906f9d
Moris M <moris.iarossi@travelfactory.fr>
2021-11-22 22:40:11 +0200

    fix  light mode

--- components/Header.vue
+++ components/Header.vue
@@ -1,31 +1,3 @@
-<template>
-  <header class="py-5 bg-black-custom">
-    <ul>
-      <li><NuxtLink class="text-white" to="/" title="home">Home</NuxtLink></li>
-      <li class="whitespace-nowrap	">
-        <NuxtLink id="zzz" class="text-white" to="/contact" title="contact"
-          >Contact me</NuxtLink
-        >
-      </li>
-    </ul>
-    <div class="d-block w-6/12">
-    <div class="pt-3">
-      <div class="flex justify-end items-center space-x-2 mx-auto relative">
-        <span class="text-xs text-white">Light </span>
-        <div>
-          <label class="switch">
-            <input type="checkbox" id="checkbox" checked />
-            <span class="slider round"></span>
-          </label>
-        </div>
-        <span class="text-xs font-semibold text-white">Dark</span>
-      </div>
-    </div>
-    </div>
-
-  </header>
-</template>
-
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;300;400;500;700&display=swap");
 
@@ -113,6 +85,34 @@ input:checked + .slider:before {
 }
 </style>
 
+<template>
+  <header class="py-5 bg-black-custom">
+    <ul>
+      <li><NuxtLink class="text-white" to="/" title="home">Home</NuxtLink></li>
+      <li class="whitespace-nowrap	">
+        <NuxtLink id="zzz" class="text-white" to="/contact" title="contact"
+          >Contact me</NuxtLink
+        >
+      </li>
+    </ul>
+    <div class="d-block w-6/12">
+    <div class="pt-3">
+      <div class="flex justify-end items-center space-x-2 mx-auto relative">
+        <span class="text-xs text-white">Light </span>
+        <div>
+          <label class="switch">
+            <input v-on:click="switcha" type="checkbox" id="checkbox" checked />
+            <span class="slider round"></span>
+          </label>
+        </div>
+        <span class="text-xs font-semibold text-white">Dark</span>
+      </div>
+    </div>
+    </div>
+
+  </header>
+</template>
+
 <script>
 export default {
   methods: {
@@ -123,55 +123,67 @@ export default {
         checkbox.addEventListener("change", () => {
             let theme = localStorage.getItem("data-theme"); // Retrieve saved them from local storage
             if (theme === "dark") {
-                console.log('dddd')
                 if (img != null) {
                     img.classList.remove("light-mode");
                 }
             } else {
-                console.log('dddd')
                 if (img != null) {
                     img.classList.add("light-mode");
                 }
             }
         });
     },
+    switcha() {
+      let theme = localStorage.getItem("data-theme");
+      const checkbox = document.getElementById("checkbox"); 
+
+      const changeThemeToDark = () => {
+        document.documentElement.setAttribute("data-theme", "light"); // set theme to dark
+        localStorage.setItem("data-theme", "dark"); // save theme to local storage
+      };
+
+      const changeThemeToLight = () => {
+        document.documentElement.setAttribute("data-theme", "dark"); // set theme light
+        localStorage.setItem("data-theme", "light"); // save theme to local storage
+      };
+
+      // Get the element based on ID
+      // Apply retrived them to the website
+      
+          
+        if (theme === "dark") {
+          changeThemeToLight();
+          checkbox.setAttribute("checked", "checked");
+        } else {
+          changeThemeToDark();
+          checkbox.removeAttribute("checked"); 
+        }
+     
+
+    }
   },
   mounted() {
     let theme = localStorage.getItem("data-theme");
-    const checkbox = document.getElementById("checkbox");    
-    // this.imgMode()
+    const checkbox = document.getElementById("checkbox");
+          const changeThemeToDark = () => {
+        document.documentElement.setAttribute("data-theme", "light"); // set theme to dark
+        localStorage.setItem("data-theme", "dark"); // save theme to local storage
+      };
 
+      const changeThemeToLight = () => {
+        document.documentElement.setAttribute("data-theme", "dark"); // set theme light
+        localStorage.setItem("data-theme", "light"); // save theme to local storage
+      };
+    console.log('mounted', theme)
     if (theme === "dark") {
-      // changeThemeToLight()
-      checkbox.setAttribute("checked", "checked");
+      changeThemeToDark()
+      checkbox.removeAttribute("checked");      
       // img.classList.remove("light-mode");
     } else {
-      checkbox.removeAttribute("checked");
-      // changeThemeToDark()      
+      changeThemeToLight()
+            checkbox.setAttribute("checked", "checked");
       // img.classList.add("light-mode");
     }
-
-    const changeThemeToDark = () => {
-      document.documentElement.setAttribute("data-theme", "light"); // set theme to dark
-      localStorage.setItem("data-theme", "dark"); // save theme to local storage
-    };
-
-    const changeThemeToLight = () => {
-      document.documentElement.setAttribute("data-theme", "dark"); // set theme light
-      localStorage.setItem("data-theme", "light"); // save theme to local storage
-    };
-
-    // Get the element based on ID
-    // Apply retrived them to the website
-    checkbox.addEventListener("change", () => {
-        
-      let theme = localStorage.getItem("data-theme"); // Retrieve saved them from local storage
-      if (theme === "dark") {
-        changeThemeToLight();
-      } else {
-        changeThemeToDark();
-      }
-    });
   },
 };
 </script>
